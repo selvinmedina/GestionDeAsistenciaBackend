ef54b2f4a233288040c26cef5f35c776319973d4
sanchezjavier <Sanchezjavier777@gmail.com>
2023-11-23 00:03:24 -0600

    Quitando algunas cosas

--- VisitServiceBackend/VisitService.Api/Program.cs
+++ VisitServiceBackend/VisitService.Api/Program.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,36 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.Authority = "http://auth-service-api/identity"; // Replace with your auth API URL
-            options.Audience = "visitapi"; // Replace with your API's audience
-            options.RequireHttpsMetadata = false; // Set to true for production environments
-        });
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Please enter into field the Bearer token",
-        Name = "Bearer",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey
-    });
-
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
-            },
-            new List<string>()
-        }
-    });
-});
 
 var app = builder.Build();
 
